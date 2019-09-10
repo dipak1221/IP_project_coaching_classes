aac1071cb2e010b7d2a1c35a9e917aa0bdc7d343
dipak122 <dipak717gautam@gmail.com>
2019-09-10 20:09:33 +0530

    login added

--- IP_project/pages/views.py
+++ IP_project/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 # Create your views here.
+
+def notice(request):
+    return render(request,'notice.html')
